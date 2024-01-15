c37fea8a7f3238cc5dabafb26ed0a715feee55fe
Lawal Opeyemi Lawal <opee2005ng@yahoo.com>
2024-01-15 15:36:32 +0100

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello World! this is Lawal and here is my first app on cloud));
+app.get('/', (req, res) => res.send('This is Lawal and here is my first app on cloud));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
